42f32a1e9d09513da7f6a76b6d56dd8c910f0ac4
redpanda518 <kevin_519@163.com>
2019-01-04 17:31:36 +0800

    删掉测试的py文件

--- test.py
+++
@@ -1,8 +0,0 @@
-def list_filesname(argPath):
-    import os
-    for sChild in os.listdir(argPath):
-        sChildPath=os.path.join(argPath,sChild)
-        if os.path.isdir(sChildPath):
-            list_filesname(sChildPath)
-        else:
-            print(sChildPath)
